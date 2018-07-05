c3257dbbeaeb1196866c9d72de756d915423da93
H S Umer Farooq <h.s.umer.farooq@gmail.com>
2018-07-06 00:47:56 +0500

    After adding deny_domains and allow domains code

--- books_toscrape/spiders/books.py
+++ books_toscrape/spiders/books.py
@@ -8,8 +8,14 @@ class BooksSpider(CrawlSpider):
     start_urls = ['http://books.toscrape.com/']
     #extract all the urls which are in index page and send requests but dont follow their urls
     #rules = (Rule(LinkExtractor(),callback='parse_page',follow=False),)
+
     # extract all the urls which are in index page and send requests, follow their urls too
-    rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
+    #rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
+
+    # it will deny domains which are passed in arguments
+    # rules = (Rule(LinkExtractor(deny_domains=('google.com')), callback='parse_page', follow=True),)
+    #this will only extact the urls which contans music in their url
+    rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=True),)
 
     def parse_page(self, response):
         pass
