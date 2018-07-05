da45e4a669e1a1d1949ca4acb9c3af0f9ecff1bd
H S Umer Farooq <h.s.umer.farooq@gmail.com>
2018-07-06 00:34:09 +0500

    Send requests to urls but not follow

--- books_toscrape/spiders/books.py
+++ books_toscrape/spiders/books.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class BooksSpider(scrapy.Spider):
+class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
-
-    def parse(self, response):
+    #extract all the urls which are in index page and send requests but dont follow their urls
+    rules = (Rule(LinkExtractor(),callback='parse_page',follow=False),)
+    def parse_page(self, response):
         pass
